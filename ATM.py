3ef75d2a6c07093508b1100e5419c99bb7472db0
DESKTOP-OP0JG4T\user <stellaj0511@gmail.com>
2021-07-11 22:51:53 +0900

    ATM
    동전의 종류 n과 돈의 가치의 합 k, 각 동전의 가치들을 입력 받았을 때 k를 만드는 데 필요한 동전의 개수의 최솟값을 출력하는 코드

---
+++ ATM.py
@@ -0,0 +1,11 @@
+# 그리디 알고리즘
+n = int(input())  # atm 이용할 사람 수
+time = list(map(int, input().split())) # 각 사람이 기다리는 시간
+time.sort()
+sum_t = 0
+k = 0
+for i in range(len(time)):  # 각 사람이 돈을 인출하는 데 필요한 시간의 최소합
+    k += time[i]
+    sum_t += k
+print(sum_t)
+    
