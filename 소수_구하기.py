e30b8e585036e1146719df960c53bd78660bb9e3
DESKTOP-OP0JG4T\user <stellaj0511@gmail.com>
2021-07-23 22:37:22 +0900

    소수_구하기

---
+++ 소수_구하기.py
@@ -0,0 +1,12 @@
+def isPrime(num):
+    if num == 1: return False
+    for j in range(2, int(num ** 0.5) + 1):
+        print(j, end = ' ')
+        if num % j == 0:
+            return False
+        return True
+
+m, n = map(int,input().split())
+for i in range(m, n + 1):
+    if isPrime(i):
+        print(i)
